3fe9d5232004039fce5af5a16d942ce605af41bd
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2020-04-18 12:03:14 +0100

    Package setup

---
+++ setup.py
@@ -0,0 +1,71 @@
+####################################################
+#
+# This Work is written by Nikolai Rozanov <nikolai>
+#
+# Contact:  nikolai.rozanov@gmail.com
+#
+# Copyright (C) 2018-Present Nikolai Rozanov
+#
+####################################################
+
+####################################################
+# IMPORT STATEMENTS
+####################################################
+
+# >>>>>>  Native Imports  <<<<<<<
+from setuptools import setup, find_packages
+
+# >>>>>>  Package Imports <<<<<<<
+
+# >>>>>>  Local Imports   <<<<<<<
+
+
+####################################################
+# CODE
+####################################################
+
+
+with open("README.md", "r") as fh:
+    longDescription = fh.read()
+
+
+####################################################
+# MAIN
+####################################################
+setup(
+
+      #
+      # SETUP
+      #
+
+      name          = 'rlenvs',
+      version       = '0.0.0.1',
+      url           = 'https://github.com/ai-nikolai/rl-environments',
+      author        = 'nikolai rozanov',
+      author_email  = 'nikolai.rozanov@gmail.com',
+      license       = 'Apache 2.0',
+
+      #
+      # Actual packages, data and scripts
+      #
+
+      packages      = find_packages(),
+
+      package_dir   = { 'rlenvs' : 'rlenvs' },
+
+      install_requires=[
+                        "numpy",
+                        "matplotlib",
+                        "seaborn>=0.9",
+                       ],
+
+      python_requires='>=3.6',
+
+      #
+      # DESCRIPTION
+      #
+      description   ='Reinforcment Learning Environments',
+      long_description=longDescription,
+      long_description_content_type="text/markdown",
+    )
+# EOF
